aa0d7bc96f67874a9157fdfdae5593650b53efac
pandaCSD <1480484467@qq.com>
2024-06-03 21:15:29 +0800

    修改

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" app permanent>
-      <v-list>
+      <!-- <v-list>
         <v-list-item
           v-for="item in menuItems"
           :key="item.title"
@@ -11,7 +11,35 @@
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
+      </v-list> -->
+      <v-list>
+      <v-list-item-group>
+        <v-list-item 
+          v-for="item in menuItems" 
+          :key="item.title"
+          @click="currentComponent = item.component">
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-action v-if="item.children">
+            <v-icon>mdi-chevron-down</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-action v-else>
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-group v-if="item.children">
+            <v-list-item v-for="(child, i) in item.children" :key="i">
+              <v-list-item-content>
+                <v-list-item-title v-text="child.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -44,7 +72,15 @@ export default {
       menuItems: [
         { title: 'UserPage', component: 'UserPage' },
         // 这里可以添加更多的菜单项
-        { title: 'WelcomePage', component: 'WelcomePage'}
+        { title: 'WelcomePage', component: 'WelcomePage'},
+        {
+          title: 'A栏目',
+          icon: 'mdi-folder',
+          children: [
+            { title: 'UserPage', component: 'UserPage' },
+            { title: 'WelcomePage', component: 'WelcomePage'},
+          ]
+        },
       ],
     };
   },
